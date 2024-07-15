eb7929c95566c37c6bd908d4c2902f28eca628b6
Adam Sher <adam@shernet.com>
2024-07-15 16:16:44 -0400

    Initial work

---
+++ identimetrics_downloader/__main__.py
@@ -0,0 +1,3 @@
+from . import identimetricsdownloader
+
+identimetricsdownloader.main()
---
+++ identimetrics_downloader/identimetricsdownloader.py
@@ -0,0 +1,6 @@
+# Main function
+# This sets up the QML UI  with Main.qml and starts the app
+def main():
+    print("Identimetrics Downloader")
+if __name__ == "__main__":
+   main()
